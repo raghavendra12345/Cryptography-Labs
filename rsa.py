3cd7a03db3ca2afc78d9d1141ce93735c99547c6
Raghavendra Vernekar <rnsvernekar@gmail.com>
2023-11-14 11:29:13 +0530

    Update rsa.py

--- rsa.py
+++ rsa.py
@@ -5,29 +5,25 @@ def RSA(p: int, q: int, message: int):
     # calculating n
     n = p * q
 
-    # calculating totient, t
+   
     t = (p - 1) * (q - 1)
 
-    # selecting public key, e
-    for i in range(2, t):
+       for i in range(2, t):
         if gcd(i, t) == 1:
             e = i
             break
     
-    # selecting private key, d
-    j = 0
+       j = 0
     while True:
         if (j * e) % t == 1:
             d = j
             break
         j += 1
-
-    # performing encryption
+    
     ct = (message ** e) % n
     print(f"Encrypted message is {ct}")
 
-    # performing decryption
-    mes = (ct ** d) % n
+        mes = (ct ** d) % n
     print(f"Decrypted message is {mes}")
 
 # Testcase - 1
